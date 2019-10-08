5e79d3c1a48f983680092c47beff50dadba8c907
Elmar <ielmar@Elmars-MacBook-Pro.local>
2019-10-08 21:58:52 +1400

    cron jobs

--- cron.js
+++ cron.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import mysql from 'mysql';
 import connection  from './lib/db';
 
-cron.schedule("1 0 * * *", function() {
+cron.schedule("* * * * *", function() {
     console.log("running a task every day at night");
 
     connection.query('select device_id from (select *, sum(`test_point`) as total from yolnisanlari_contest_info where date(test_date) = date(now())-1 group by device_id having sum(`test_point`) >= 1000) t order by rand() limit 1', function(err, result) {
