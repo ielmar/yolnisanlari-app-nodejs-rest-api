23eb867de5b0fc84903be9a229c8066cc0c77ee5
Elmar <ielmar@Elmars-MacBook-Pro.local>
2019-10-08 22:23:28 +1400

    cron winner

--- cron.js
+++ cron.js
@@ -6,13 +6,17 @@ import connection  from './lib/db';
 cron.schedule("* * * * *", function() {
     console.log("running a task every day at night");
 
-    connection.query('select device_id from (select *, sum(`test_point`) as total from yolnisanlari_contest_info where date(test_date) = date(now())-1 group by device_id having sum(`test_point`) >= 1000) t order by rand() limit 1', function(err, result) {
+    connection.query('select device_id, date(test_date) AS win_date from (select *, sum(`test_point`) as total from yolnisanlari_contest_info where date(test_date) = date(now())-1 group by device_id having sum(`test_point`) >= 1000) t order by rand() limit 1', function(err, result) {
         if(err) throw err
-        if (err) {
-
-              
-        } else {                
-            console.log(result[0].device_id)
+             
+        var dailyWinner = {
+            device_id: result[0].device_id,
+            win_date: result[0].win_date,
+            daily: 1
         }
-      })
+        connection.query('INSERT INTO yolnisanlari_winners SET ?', dailyWinner, function(err, result) {
+            if(err) throw err
+            console.log(result[0].device_id+' is the winner')
+        })
+    })
 });
