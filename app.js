ba0e64ec5bb7b4a98cb0d6a43571891283b4a7ed
Elmar <ielmar@Elmars-MacBook-Pro.local>
2019-10-16 14:35:50 +0800

    api changes

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ app.post('/api/v1/getDailyWinner', (req, res) => {
 
             return res.status(201).send({
               success: 'true',
-              message: 'Təbriklər! Bugünkü qalib sənsən! Dostlarınla paylaş, dostların da həm öyrənib, həm balans əldə etsinlər!',
+              message: 'Təbriklər! Bugünkü qalib sənsən! \n\r Dostlarınla paylaş, dostların da həm öyrənib, həm balans əldə etsinlər!',
               winner 
             })
           })
