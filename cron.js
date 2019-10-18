7ee38a312c4933f0db8d94678f5001fa1aa75115
Elmar <ielmar@Elmars-MacBook-Pro.local>
2019-10-18 23:37:58 +0800

    more than 0 video requirement added to cron

--- cron.js
+++ cron.js
@@ -6,7 +6,7 @@ import connection  from './lib/db';
 cron.schedule("10 0 * * *", function() {
     console.log("running a task to select the winner for yesterday");
 
-    connection.query('select device_id, date(test_date) AS win_date from (select *, sum(`test_point`) as total from yolnisanlari_contest_info where date(test_date) = date(now())-1 group by device_id having sum(`test_point`) >= 1000) t order by rand() limit 1', function(err, result) {
+    connection.query('select device_id, date(test_date) AS win_date from (select *, sum(`test_point`) as total from yolnisanlari_contest_info where date(test_date) = date(now())-1 group by device_id having sum(`test_point`) >= 1000 AND sum(`watched_video`) > 0) t order by rand() limit 1', function(err, result) {
         if(err) throw err
         var winner_id = result[0].device_id
              
